9ffb3325cea2ea7bda478d6707ce4426adac1e5d
nelesha-alt <kosse96@mail.ru>
2023-05-04 15:13:34 +0300

    test2

--- projectvue/src/App.vue
+++ projectvue/src/App.vue
@@ -5,16 +5,23 @@
 <template>
   <div class="app">
     <form>
-      <h1>MAS</h1>
+      <h1 class="multi">
+        <span id="m">M</span>
+        <span id="a">A</span>
+        <span id="s">S</span>
+      </h1>
       <p>Log in Sing up to access your account</p>
         <div class="input-container">
-      <label for="phoneNumber">+7</label>
-      <input id="phoneNumber" v-model="myValue" />
+          <label for="phoneNumber">+7</label>
+          <input id="phoneNumber" v-model="myValue" class="input"/>
+          <div class="btn">
+            <button @click="click" class="cnt">Continue</button>
+          </div>
         </div>
     </form>
   </div>
 </template>
 
-<style>
+<style scoped>
    
 </style>
