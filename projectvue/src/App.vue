d55dbaf70f35eacb378ab48a759ec1bba695e3d1
nelesha-alt <kosse96@mail.ru>
2023-05-04 13:56:50 +0300

    project

---
+++ projectvue/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="app">
+    <form>
+      <h1>MAS</h1>
+      <p>Log in Sing up to access your account</p>
+        <div class="input-container">
+      <label for="phoneNumber">+7</label>
+      <input id="phoneNumber" v-model="myValue" />
+        </div>
+    </form>
+  </div>
+</template>
+
+<style>
+   
+</style>
